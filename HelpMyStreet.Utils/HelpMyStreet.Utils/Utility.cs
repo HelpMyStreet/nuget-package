1983e8e90754e2778b1641db0401b41c3304cbb4
Mike Matthews <mike@factor-50.co.uk>
2020-03-29 22:43:09 +0100

    Auto stash before revert of "Merge pull request #5 from HelpMyStreet/dotnetpack"

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Utility.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Utility.cs
@@ -4,11 +4,6 @@ namespace HelpMyStreet.Utils
 {
     public class Utility
     {
-        public string HelloWorld()
-        {
-            return "Hello world";
-        }
-
         public void SharedTestFunction()
         {
             throw  new NotImplementedException();

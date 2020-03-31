55733ff2c5f31a9151311e372c477172c3997653
Mike Matthews <mike@factor-50.co.uk>
2020-03-31 17:05:18 +0100

    Adding User, UserPersonalDetails, Address

---
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Models/UserPersonalDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Utils.Models
+{
+    public class UserPersonalDetails
+    {
+        public User User { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DisplayName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Address Address { get; set; }
+        public string EmailAddress { get; set; }
+        public string MobilePhone { get; set; }
+        public string OtherPhone { get; set; }
+    }
+}

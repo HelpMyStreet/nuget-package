81116aaedbb1df16c72c665a4663b506c54256a8
Jawwad Mukhtar <jawwad.mukhtar@gmail.com>
2020-04-02 13:53:36 +0100

    Make parameters optional

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Models/User.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Models/User.cs
@@ -9,13 +9,13 @@ namespace HelpMyStreet.Utils.Models
         public int ID { get; set; }
         public string FirebaseUID { get; set; }
         public string PostalCode { get; set; }
-        public bool EmailSharingConsent { get; set; }
-        public bool MobileSharingConsent { get; set; }
-        public bool OtherPhoneSharingConsent { get; set; }
-        public bool HMSContactConsent { get; set; }
-        public bool IsVolunteer { get; set; }
-        public bool IsVerified { get; set; }
-        public DateTime DateCreated { get; set; }
+        public bool? EmailSharingConsent { get; set; }
+        public bool? MobileSharingConsent { get; set; }
+        public bool? OtherPhoneSharingConsent { get; set; }
+        public bool? HMSContactConsent { get; set; }
+        public bool? IsVolunteer { get; set; }
+        public bool? IsVerified { get; set; }
+        public DateTime? DateCreated { get; set; }
         public UserPersonalDetails UserPersonalDetails { get; set; }
         public Dictionary<int, DateTime> RegistrationHistory { get; set; }
     }
--- HelpMyStreet.Utils/HelpMyStreet.Utils/Models/UserPersonalDetails.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Models/UserPersonalDetails.cs
@@ -10,7 +10,7 @@ namespace HelpMyStreet.Utils.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string DisplayName { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public DateTime? DateOfBirth { get; set; }
         public Address Address { get; set; }
         public string EmailAddress { get; set; }
         public string MobilePhone { get; set; }

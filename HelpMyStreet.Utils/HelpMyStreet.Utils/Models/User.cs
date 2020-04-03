eed7445d07d7a1c30ffb53d8596e94ab9fee2729
Mike Matthews <mike@factor-50.co.uk>
2020-04-03 12:20:04 +0100

    Adding missing fields, and comments relating to registration steps

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Models/User.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Models/User.cs
@@ -6,16 +6,42 @@ namespace HelpMyStreet.Utils.Models
 {
     public class User
     {
+        // Generated in database
         public int ID { get; set; }
+
+
+        // Supplied in registration step 1
         public string FirebaseUID { get; set; }
+        public DateTime DateCreated { get; set; }
+
+
+        // Supplied in registration step 2
         public string PostalCode { get; set; }
+
+
+        // Supplied in registration step 3
+         public List<int> SupportActivities { get; set; }
+        public float SupportRadiusMiles { get; set; }
+        public bool SupportVolunteersByPhone { get; set; }
+        public bool UnderlyingMedicalCondition { get; set; }
+
+        // Automatically set to true following registration step 3
+        public bool IsVolunteer { get; set; }
         public bool EmailSharingConsent { get; set; }
         public bool MobileSharingConsent { get; set; }
         public bool OtherPhoneSharingConsent { get; set; }
         public bool HMSContactConsent { get; set; }
-        public bool IsVolunteer { get; set; }
+
+
+        // Supplied in registration step 4
+        public bool StreetChampionRoleUnderstood { get; set; }
+        public List<string> ChampionPostcodes { get; set; }
+
+
+        // Supplied in registration step 5
         public bool IsVerified { get; set; }
-        public DateTime DateCreated { get; set; }
+
+
         public UserPersonalDetails UserPersonalDetails { get; set; }
         public Dictionary<int, DateTime> RegistrationHistory { get; set; }
     }
--- HelpMyStreet.Utils/HelpMyStreet.Utils/Models/UserPersonalDetails.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Models/UserPersonalDetails.cs
@@ -7,13 +7,23 @@ namespace HelpMyStreet.Utils.Models
     public class UserPersonalDetails
     {
         public User User { get; set; }
+
+        // Supplied in registration step 1
+        public string EmailAddress { get; set; }
+
+
+        // Supplied in registration step 2
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string DisplayName { get; set; }
-        public DateTime DateOfBirth { get; set; }
         public Address Address { get; set; }
-        public string EmailAddress { get; set; }
         public string MobilePhone { get; set; }
         public string OtherPhone { get; set; }
+
+        // Defaulted to FirstName following step 2
+        public string DisplayName { get; set; }
+
+
+        // Supplied in registration step 3
+        public DateTime DateOfBirth { get; set; }
     }
 }

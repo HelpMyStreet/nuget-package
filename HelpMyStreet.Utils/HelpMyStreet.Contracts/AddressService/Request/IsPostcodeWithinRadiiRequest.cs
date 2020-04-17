fa63f55b018c4210af2e9bb2a8b5e6d5a8f256e3
William Thomas <14827486+william-thomas@users.noreply.github.com>
2020-04-17 19:09:06 +0100

    Add contracts for IsPodtcodeWithinRadii endpoint

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/AddressService/Request/IsPostcodeWithinRadiiRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using MediatR;
+
+namespace HelpMyStreet.Contracts.AddressService.Request
+{
+    public class IsPostcodeWithinRadiiRequest : IRequest<IsPostcodeWithinRadiiResponse>
+    {
+        [Required]
+        public string Postcode { get; set; }
+
+        [Required]
+        public List<PostcodeWithRadius> PostcodeWithRadiuses { get; set; }
+
+    }
+}

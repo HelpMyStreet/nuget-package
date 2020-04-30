37454f6ae8ca81cb178d4ee6681ede378a9d9144
William Thomas <14827486+william-thomas@users.noreply.github.com>
2020-04-30 17:00:57 +0100

    Polly retry policies.  Separate pause config is used for 503s for internal services because this indicates an Azure Function has stopped running due to inactivity.

---
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/PollyPolicies/IPollyHttpPolicies.cs
@@ -0,0 +1,22 @@
+﻿using Polly.Retry;
+using Polly.Wrap;
+using System.Net.Http;
+
+namespace HelpMyStreet.Utils.PollyPolicies
+{
+    /// <summary>
+    /// HTTP retry policies. This has a dependency on IPollyHttpPoliciesConfig so it must be registered in the DI (PollyHttpPoliciesConfig is an implementation).
+    /// </summary>
+    public interface IPollyHttpPolicies
+    {
+        /// <summary>
+        /// Retries on 408 and 5XX errors.  503 errors have a different config for the pause between retires to allow time for the Azure Function to start up.
+        /// </summary>
+        AsyncPolicyWrap<HttpResponseMessage> InternalHttpRetryPolicy { get; }
+
+        /// <summary>
+        /// Retries on 408 and 5XX errors.
+        /// </summary>
+        AsyncRetryPolicy<HttpResponseMessage> ExternalHttpRetryPolicy { get; }
+    }
+}
---
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/PollyPolicies/PollyHttpPolicies.cs
@@ -0,0 +1,35 @@
+﻿using Polly;
+using Polly.Retry;
+using Polly.Wrap;
+using System.Net;
+using System.Net.Http;
+
+namespace HelpMyStreet.Utils.PollyPolicies
+{
+    /// <inheritdoc />>
+    public class PollyHttpPolicies : IPollyHttpPolicies
+    {
+        /// <inheritdoc />>
+        public AsyncPolicyWrap<HttpResponseMessage> InternalHttpRetryPolicy { get; }
+
+        /// <inheritdoc />>
+        public AsyncRetryPolicy<HttpResponseMessage> ExternalHttpRetryPolicy { get; }
+
+        public PollyHttpPolicies(IPollyHttpPoliciesConfig pollyHttpPoliciesConfig)
+        {
+            AsyncRetryPolicy<HttpResponseMessage> otherHttpRetryPolicy = Policy
+                .HandleResult<HttpResponseMessage>(message => message.StatusCode != HttpStatusCode.ServiceUnavailable && (message.StatusCode == HttpStatusCode.RequestTimeout || ((int)message.StatusCode >= 500 && (int)message.StatusCode < 600))) // HTTP errors that don't indicate that Azure Function not running
+                .WaitAndRetryAsync(pollyHttpPoliciesConfig.ServiceErrorPauses);
+
+            AsyncRetryPolicy<HttpResponseMessage> azureFunctionHttpsRetryPolicy = Policy
+                .HandleResult<HttpResponseMessage>(message => message.StatusCode == HttpStatusCode.ServiceUnavailable) // HTTP error that indicates Azure Function not running
+                .WaitAndRetryAsync(pollyHttpPoliciesConfig.AzureFunctionNotStartedPauses);
+
+            InternalHttpRetryPolicy = Policy.WrapAsync(otherHttpRetryPolicy, azureFunctionHttpsRetryPolicy);
+            
+            ExternalHttpRetryPolicy = Policy
+                .HandleResult<HttpResponseMessage>(message => message.StatusCode == HttpStatusCode.RequestTimeout || ((int)message.StatusCode >= 500 && (int)message.StatusCode < 600))
+                .WaitAndRetryAsync(pollyHttpPoliciesConfig.ServiceErrorPauses);
+        }
+    }
+}

0c05b96a1f5e7555256031c883cfcefff27b9424
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-05-22 16:10:42 +0100

    Add new contracts

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/UserService/Response/GetUsersByFilterResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Contracts.UserService.Response
+{
+    public class GetUsersByFilterResponse
+    {
+        public IEnumerable<UserSummary> UserSummaries { get; set; }
+    }
+}
---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/UserService/Response/UserSummary.cs
@@ -0,0 +1,20 @@
+﻿using HelpMyStreet.Utils.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Contracts.UserService.Response
+{
+    public class UserSummary
+    {
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PostCode { get; set; }
+        public bool? IsVolunteer { get; set; }
+        public bool? IsVerified { get; set; }
+        public bool? IsStreetChampion { get; set; }
+        public bool? IsStreetChampionForGivenPostCode { get; set; }
+
+    }
+}

7e1b409ac7e1e52654be10a7065ad8de8f66a2cf
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-05-22 09:14:04 +0100

    GetUsersResponse

--- HelpMyStreet.Utils/HelpMyStreet.Contracts/UserService/Response/GetUsersResponse.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/UserService/Response/GetUsersResponse.cs
@@ -6,13 +6,6 @@ namespace HelpMyStreet.Contracts.UserService.Response
 {
     public class GetUsersResponse
     {
-        public int UserID { get; set; }
-        public bool IsVolunteer { get; set; }
-        public bool IsVerified { get; set; }
-        public bool IsStreetChampion { get; set; }
-        public string EmailAddress { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string PostCode { get; set; }
+        public IEnumerable<UserDetails> UserDetails { get; set; }
     }
 }

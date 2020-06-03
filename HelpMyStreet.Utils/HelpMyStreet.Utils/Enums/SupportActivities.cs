58c71c8aaa4304439259cd56d6eeac5b10255726
Usmaan Razaq <usrazaq@gmail.com>
2020-06-03 09:45:54 +0100

    added well being package

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Enums/SupportActivities.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Enums/SupportActivities.cs
@@ -37,8 +37,12 @@ namespace HelpMyStreet.Utils.Enums
         //Reaching out to people in my area to check-in
 
         Other = 11,
-
-        FaceMask = 12
         //I may be able to help with other tasks. Please check with me.
+
+        FaceMask = 12,
+        //making a face covering
+
+        WellbeingPackage = 13
+        //Age UK - Collecting and delivering a pre-prepared wellbeing package
     }
 }

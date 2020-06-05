738f3f57e5858bb8592b575549ea12fed68f8a4c
Usmaan Razaq <usrazaq@gmail.com>
2020-06-05 16:13:25 +0100

    made public

--- HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Response/GetQuestionsByActivtiesResponse.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Response/GetQuestionsByActivtiesResponse.cs
@@ -8,6 +8,6 @@ namespace HelpMyStreet.Contracts.RequestService.Response
 {
    public class GetQuestionsByActivtiesResponse
     {
-        Dictionary<SupportActivities, List<Question>> SupportActivityQuestions { get; set; }
+       public Dictionary<SupportActivities, List<Question>> SupportActivityQuestions { get; set; }
     }
 }

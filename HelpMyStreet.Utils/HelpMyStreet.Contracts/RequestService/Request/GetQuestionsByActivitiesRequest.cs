1c6a87ea343b22638140f898b1be6ecbd425b26f
Usmaan Razaq <usrazaq@gmail.com>
2020-06-05 15:57:27 +0100

    adding IRequest

--- HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Request/GetQuestionsByActivitiesRequest.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Request/GetQuestionsByActivitiesRequest.cs
@@ -1,11 +1,12 @@
 ﻿using HelpMyStreet.Utils.Enums;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HelpMyStreet.Contracts.RequestService.Request
 {
-    public class GetQuestionsByActivitiesRequest
+    public class GetQuestionsByActivitiesRequest : IRequest
     {
         public List<SupportActivities> SupportActivities {get;set;}
     }

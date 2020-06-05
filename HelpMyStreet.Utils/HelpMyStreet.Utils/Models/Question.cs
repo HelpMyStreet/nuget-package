159f32308acd22d0efa4e36806d4000173ff0061
Usmaan Razaq <usrazaq@gmail.com>
2020-06-05 15:46:18 +0100

    removed value

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Models/Question.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Models/Question.cs
@@ -9,7 +9,6 @@ namespace HelpMyStreet.Utils.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Value { get; set; }
         public QuestionType Type {get;set;}
         public bool Required { get; set; }
         public string Answer { get; set; }

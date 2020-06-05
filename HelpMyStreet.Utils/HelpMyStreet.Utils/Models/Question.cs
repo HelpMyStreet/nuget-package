0c54d5be5195418590a1c53b6fcbd5501b7dea84
Usmaan Razaq <usrazaq@gmail.com>
2020-06-05 19:02:10 +0100

    added additional question data class

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Models/Question.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Models/Question.cs
@@ -12,6 +12,8 @@ namespace HelpMyStreet.Utils.Models
         public QuestionType Type {get;set;}
         public bool Required { get; set; }
         public string Answer { get; set; }
-        public string AddtitonalData { get; set; }
+        public List<AdditonalQuestionData> AddtitonalData { get; set; }
     }
+
+
 }

512c9ad03af61064c506a9d0e5fc4cbf299b4759
Usmaan Razaq <usrazaq@gmail.com>
2020-06-05 15:39:09 +0100

    addded answer to question

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Models/Question.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Models/Question.cs
@@ -12,5 +12,6 @@ namespace HelpMyStreet.Utils.Models
         public string Value { get; set; }
         public QuestionType Type {get;set;}
         public bool Required { get; set; }
+        public string Answer { get; set; }
     }
 }

62b8137c9134078cda9ed5c22441b4e8da460776
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-06-08 17:01:21 +0100

    Add new objects for UserService

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/UserService/Response/UserRegistrationStep.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Contracts.UserService.Response
+{
+    public class UserRegistrationStep
+    {
+        public int RegistrationStep { get; set; }
+        public int UserId { get; set; }
+    }
+}

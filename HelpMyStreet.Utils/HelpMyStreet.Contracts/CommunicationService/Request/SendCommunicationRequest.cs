fa37bbeb6bf680aff69d54b13b2bc17496b3597d
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-06-08 10:18:05 +0100

    Add objects

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/CommunicationService/Request/SendCommunicationRequest.cs
@@ -0,0 +1,18 @@
+﻿using HelpMyStreet.Contracts.CommunicationService.Response;
+using HelpMyStreet.Contracts.RequestService.Response;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Contracts.CommunicationService.Request
+{
+    public class SendCommunicationRequest : IRequest<SendCommunicationResponse>
+    {
+        public CommunicationJobs CommunicationJobs { get; set; }
+        public int? RecipientUserID { get; set; }
+        public int? JobID { get; set; }
+        public int? GroupID { get; set; }
+        public Dictionary<string, string> AdditionalParameters { get; set; }
+    }
+}
---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/CommunicationService/Request/SendMessageRequest.cs
@@ -0,0 +1,20 @@
+﻿using HelpMyStreet.Contracts.CommunicationService.Response;
+using HelpMyStreet.Contracts.RequestService.Response;
+using HelpMyStreet.Utils.Enums;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Contracts.CommunicationService.Request
+{
+    public class SendMessageRequest
+    {
+        public MessageType MessageType { get; set; }
+        public string TemplateID { get; set; }
+        public int? RecipientUserID { get; set; }
+        public int? JobID { get; set; }
+        public int? GroupID { get; set; }
+        public Dictionary<string, string> AdditionalParameters { get; set; }
+    }
+}

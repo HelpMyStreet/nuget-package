85170a5258dd5153b72d20d16f5b91b7df1c964f
Mike Matthews <mike@factor-50.co.uk>
2020-06-16 17:00:40 +0100

    Adding comments, and making distance nullable

--- HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Request/GetJobsByFilterRequest.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Request/GetJobsByFilterRequest.cs
@@ -10,9 +10,28 @@ namespace HelpMyStreet.Contracts.RequestService.Request
 {
     public class GetJobsByFilterRequest : IRequest<GetJobsByFilterResponse>
     { 
-        public string Postcode { get; set; }
-        public double? DistanceInMiles { get; set; }
+        /// <summary>
+        /// Support activities to be returned
+        /// Supply null to return all activities
+        /// </summary>
         public List<SupportActivities> SupportActivities { get; set; }
-        public Dictionary<SupportActivities,double> ActivitySpecificSupportDistancesInMiles { get; set; }
+
+        /// <summary>
+        /// Base postcode for calculating distances
+        /// Required
+        /// </summary>
+        public string Postcode { get; set; }
+
+        /// <summary>
+        /// Default support distance
+        /// Supply null to return requests nationwide
+        /// </summary>
+        public double? DistanceInMiles { get; set; }
+
+        /// <summary>
+        /// Overrides DistanceInMiles for specific activities
+        /// Supply null for an activity type to return requets nationwide
+        /// </summary>
+        public Dictionary<SupportActivities,double?> ActivitySpecificSupportDistancesInMiles { get; set; }
     }
 }

d92770ef165a8afa06a8320c228a4c8d301e1d77
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-07-01 15:02:50 +0100

    Make param mandatory

--- HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Request/GetGroupByKeyRequest.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Request/GetGroupByKeyRequest.cs
@@ -1,10 +1,12 @@
 ﻿using HelpMyStreet.Contracts.GroupService.Response;
 using MediatR;
+using System.ComponentModel.DataAnnotations;
 
 namespace HelpMyStreet.Contracts.GroupService.Request
 {
     public class GetGroupByKeyRequest : IRequest<GetGroupByKeyResponse>
     {
+        [Required]
         public string GroupKey { get; set; }
     }
 

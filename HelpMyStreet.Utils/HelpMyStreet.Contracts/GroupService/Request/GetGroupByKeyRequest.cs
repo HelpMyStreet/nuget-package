d4d143d16e68c843fb664a9ab158edb44386a1e5
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-07-01 14:03:04 +0100

    Add GetGroupByKey

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Request/GetGroupByKeyRequest.cs
@@ -0,0 +1,11 @@
+﻿using HelpMyStreet.Contracts.GroupService.Response;
+using MediatR;
+
+namespace HelpMyStreet.Contracts.GroupService.Request
+{
+    public class GetGroupByKeyRequest : IRequest<GetGroupByKeyResponse>
+    {
+        public string GroupKey { get; set; }
+    }
+
+}

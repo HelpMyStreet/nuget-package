52100262b9a38c821122149a796aaec2c5424aa5
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-07-01 15:54:24 +0100

    Add community volunteer contract

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Request/GetLandingPageContentRequest.cs
@@ -0,0 +1,9 @@
+﻿using HelpMyStreet.Contracts.GroupService.Response;
+using MediatR;
+namespace HelpMyStreet.Contracts.GroupService.Request
+{
+    public class GetLandingPageContentRequest : IRequest<GetLandingPageContentResponse>
+    {
+        public int GroupID { get; set; }
+    }
+}
---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Response/GetLandingPageContentResponse.cs
@@ -0,0 +1,32 @@
+﻿using HelpMyStreet.Contracts.GroupService.Request;
+using System.Collections.Generic;
+
+namespace HelpMyStreet.Contracts.GroupService.Response
+{
+    public class GetLandingPageContentResponse
+    {
+        public bool IsLoggedIn { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public int ZoomLevel { get; set; }
+        public string CommunityName { get; set; }
+        public string BannerImageLocation { get; set; }
+        public string Header { get; set; }
+        public string HeaderSubtitle { get; set; }
+        public IEnumerable<string> HeaderBullets { get; set; }
+        public string HeaderHelpButtonText { get; set; }
+        public string HeaderVolunteerButtonText { get; set; }
+        public bool HideHeaderButtons { get; set; } = false;
+        public bool HideHeaderVolunteerButton { get; set; } = false;
+        public bool HideHeaderHelpButton { get; set; } = false;
+        public string CommunityVolunteersHeader { get; set; }
+        public string CommunityVolunteersTextHtml { get; set; }
+        public bool HideHelpPanel { get; set; } = false;
+        public string RequestHelpHeading { get; set; }
+        public string RequestHelpText { get; set; }
+        public string ProvideHelpHeading { get; set; }
+        public string ProvideHelpText { get; set; }
+        public IEnumerable<CommunityVolunteer> CommunityVolunteers { get; set; }
+        public IEnumerable<string> UsefulLinksHtml { get; set; }
+    }
+}

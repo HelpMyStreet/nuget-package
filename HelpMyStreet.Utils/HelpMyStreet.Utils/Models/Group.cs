350940a77bccdd3cfe04353b0cbf11582241f878
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-07-03 13:10:40 +0100

    Add GetGroupRequest and Response

---
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Models/Group.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Utils.Models
+{
+    public class Group
+    {
+        public string GroupName { get; set; }
+        public string GroupKey { get; set; }
+        public string ParentGroupName { get; set; }
+    }
+}

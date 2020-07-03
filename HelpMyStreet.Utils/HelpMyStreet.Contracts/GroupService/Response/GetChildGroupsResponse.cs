1775dace9ff23cb33a642549a41196de14c434a8
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-07-03 13:06:05 +0100

    Add GetChildGroups

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Response/GetChildGroupsResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Contracts.GroupService.Response
+{
+    public class GetChildGroupsResponse
+    {
+        public List<int> ChildGroups { get; set; }
+    }
+}

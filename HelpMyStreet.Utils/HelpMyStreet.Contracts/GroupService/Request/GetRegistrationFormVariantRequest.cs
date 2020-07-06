d07779d98d90b2af93075d2ddc2844fad9ce1a06
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-07-06 16:36:47 +0100

    add required

--- HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Request/GetRegistrationFormVariantRequest.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Request/GetRegistrationFormVariantRequest.cs
@@ -1,11 +1,14 @@
 ﻿using HelpMyStreet.Contracts.GroupService.Response;
 using MediatR;
+using System.ComponentModel.DataAnnotations;
 
 namespace HelpMyStreet.Contracts.GroupService.Request
 {
     public class GetRegistrationFormVariantRequest : IRequest<GetRegistrationFormVariantResponse>
     {
+        [Required]
         public int? GroupID { get; set; }
+        [Required]
         public string Source { get; set; }
     }
 }
--- HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Request/GetRequestHelpFormVariantRequest.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Request/GetRequestHelpFormVariantRequest.cs
@@ -1,11 +1,15 @@
 ﻿using HelpMyStreet.Contracts.GroupService.Response;
 using MediatR;
+using System.ComponentModel.DataAnnotations;
 
 namespace HelpMyStreet.Contracts.GroupService.Request
 {
     public class GetRequestHelpFormVariantRequest : IRequest<GetRequestHelpFormVariantResponse>
     {
+        [Required]
         public int? GroupID { get; set; }
+
+        [Required]
         public string Source { get; set; }
     }
 }

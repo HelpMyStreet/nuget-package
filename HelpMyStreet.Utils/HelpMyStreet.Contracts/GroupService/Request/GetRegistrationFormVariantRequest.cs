dc52f72fa8e2e7e511833d385bafd475bf46ec9e
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-07-06 15:39:18 +0100

    Add GetRegistrationFormVariant method

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Request/GetRegistrationFormVariantRequest.cs
@@ -0,0 +1,11 @@
+﻿using HelpMyStreet.Contracts.GroupService.Response;
+using MediatR;
+
+namespace HelpMyStreet.Contracts.GroupService.Request
+{
+    public class GetRegistrationFormVariantRequest : IRequest<GetRegistrationFormVariantResponse>
+    {
+        public int? GroupID { get; set; }
+        public string Source { get; set; }
+    }
+}
---
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Enums/RegistrationSource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Utils.Enums
+{
+    public enum RegistrationSource
+    {
+        Default = 0,
+        HLP = 1,
+        FtLOS = 2
+    }
+}
+    

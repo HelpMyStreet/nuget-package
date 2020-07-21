c9dbce0bed7d7a9a0431dd12e9f59c45260ed835
Mike Matthews <mike@factor-50.co.uk>
2020-07-21 13:49:21 +0100

    Adding QuestionType.LabelOnly

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Enums/QuestionType.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Enums/QuestionType.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace HelpMyStreet.Utils.Enums
+﻿namespace HelpMyStreet.Utils.Enums
 {
     public enum QuestionType
     {
        Number = 1,
        Text= 2,
        MultiLineText = 3,
-       Radio = 4
+       Radio = 4,
+       LabelOnly = 5,
     }
 }

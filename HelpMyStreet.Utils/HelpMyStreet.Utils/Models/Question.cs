598cfd1c6e1ee7256a07589b13de6025033ebedf
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-07-21 10:27:04 +0100

    Remove required property

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Models/Question.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Models/Question.cs
@@ -10,7 +10,6 @@ namespace HelpMyStreet.Utils.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public QuestionType Type {get;set;}
-        public bool Required { get; set; }
         public string Answer { get; set; }
         public List<AdditonalQuestionData> AddtitonalData { get; set; }
     }

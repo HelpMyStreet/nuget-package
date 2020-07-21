83cd8db9ac52c7ce82debbfea3e1b2ac97ba641c
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-07-21 16:50:48 +0100

    Add post delete user contact

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/UserService/Request/PostDeleteUserRequest.cs
@@ -0,0 +1,15 @@
+﻿using HelpMyStreet.Contracts.UserService.Response;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace HelpMyStreet.Contracts.UserService.Request
+{
+    public class PostDeleteUserRequest : IRequest<PostDeleteUserResponse>
+    {
+        [Required]
+        public int UserID { get; set; }
+
+        [Required]
+        public string Postcode { get; set; }
+    }
+}

6f58fef8f4c107170c0f4ba666316da20c243de4
Mike Matthews <mike@factor-50.co.uk>
2020-07-24 17:31:34 +0100

    Adding SubText, PlaceholderText, Location to Utils.Models.Question

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Models/Question.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Models/Question.cs
@@ -11,6 +11,9 @@ namespace HelpMyStreet.Utils.Models
         public string Name { get; set; }
         public QuestionType Type {get;set;}
         public bool Required { get; set; }
+        public string SubText { get; set; }
+        public string PlaceholderText { get; set; }
+        public string Location { get; set; }
         public string Answer { get; set; }
         public List<AdditonalQuestionData> AddtitonalData { get; set; }
     }

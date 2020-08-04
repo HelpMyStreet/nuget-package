5fc9106c2d26168bf2e05e5794dc85ed1cf50a39
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-08-04 16:22:07 +0100

    add http client wrapper

---
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Enums/HttpClientConfigName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Utils.Enums
+{
+    public enum HttpClientConfigName
+    {
+        UserService = 1,
+        CommunicationService = 2,
+        AddressService = 3,
+        GroupService = 4,
+        RequestService = 5
+    }
+}

cf8ecdcf56fe8682c173c53fd6d13bd730058471
Mike Matthews <mike@factor-50.co.uk>
2020-08-24 16:20:02 +0100

    JobStatus Friendly Names

---
+++ HelpMyStreet.Utils/HelpMyStreet.UnitTests/JobStatusExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using HelpMyStreet.Utils.Enums;
+using HelpMyStreet.Utils.Extensions;
+using NUnit.Framework;
+
+namespace HelpMyStreet.UnitTests
+{
+    class JobStatusExtensionsTests
+    {
+        [Test]
+        public void FriendlyName_InProgress()
+        {
+            Assert.AreEqual("In Progress", JobStatuses.InProgress.FriendlyName());
+        }
+
+        [Test]
+        public void FriendlyName_Done()
+        {
+            Assert.AreEqual("Done", JobStatuses.Done.FriendlyName());
+        }
+    }
+}
---
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Extensions/JobStatusExtensions.cs
@@ -0,0 +1,16 @@
+﻿using HelpMyStreet.Utils.Enums;
+
+namespace HelpMyStreet.Utils.Extensions
+{
+    public static class JobStatusExtensions
+    {
+        public static string FriendlyName(this JobStatuses status)
+        {
+            return status switch
+            {
+                JobStatuses.InProgress => "In Progress",
+                _ => status.ToString()
+            };
+        }
+    }
+}

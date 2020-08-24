60335e9ef44270c190c7d816b17f216de1d13c34
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-08-24 12:23:16 +0100

    upgrade .net core 3

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Extensions/SqlConnectionExtensions.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Extensions/SqlConnectionExtensions.cs
@@ -1,16 +1,13 @@
-﻿using System.Data.SqlClient;
-using Microsoft.Azure.Services.AppAuthentication;
-
-namespace HelpMyStreet.Utils.Extensions
+﻿namespace HelpMyStreet.Utils.Extensions
 {
     public static class SqlConnectionExtensions
     {
-        public static void AddAzureToken(this SqlConnection connection)
-        {
-            if (connection.DataSource.Contains("database.windows.net"))
-            {
-                connection.AccessToken = new AzureServiceTokenProvider().GetAccessTokenAsync("https://database.windows.net/").Result;
-            }
-        }
+        //public static void AddAzureToken(this SqlConnection connection)
+        //{
+        //    if (connection.DataSource.Contains("database.windows.net"))
+        //    {
+        //        connection.AccessToken = new AzureServiceTokenProvider().GetAccessTokenAsync("https://database.windows.net/").Result;
+        //    }
+        //}
     }
 }

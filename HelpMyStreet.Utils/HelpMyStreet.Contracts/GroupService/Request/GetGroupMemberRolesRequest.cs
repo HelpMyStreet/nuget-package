29ea0578a987e67da448d10cc9d1a532981f0435
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-08-28 12:26:44 +0100

    add contracts

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Request/GetGroupMemberRolesRequest.cs
@@ -0,0 +1,11 @@
+﻿using HelpMyStreet.Contracts.GroupService.Response;
+using MediatR;
+
+namespace HelpMyStreet.Contracts.GroupService.Request
+{
+    public class GetGroupMemberRolesRequest : IRequest<GetGroupMemberRolesResponse>
+    {
+        public int GroupId { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Response/GetGroupMemberRolesResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace HelpMyStreet.Contracts.GroupService.Response
+{
+    public class GetGroupMemberRolesResponse
+    {
+        public Dictionary<int, List<int>> GroupMemberRoles { get; set; }
+    }
+}

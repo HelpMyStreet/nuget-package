7c92137c9cca8590e5e0398f22a0f5ffe4ec7344
Mike Matthews <mike@factor-50.co.uk>
2020-08-28 09:46:11 +0100

    Adding GroupRoles.FriendlyName extension

---
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Extensions/GroupRolesExtensions.cs
@@ -0,0 +1,18 @@
+﻿using HelpMyStreet.Utils.Enums;
+
+namespace HelpMyStreet.Utils.Extensions
+{
+    public static class GroupRolesExtensions
+    {
+        public static string FriendlyName(this GroupRoles role)
+        {
+            return role switch
+            {
+                GroupRoles.UserAdmin => "User Administrator",
+                GroupRoles.TaskAdmin => "Request Administrator",
+                GroupRoles.RequestSubmitter => "Request Submitter",
+                _ => role.ToString()
+            };
+        }
+    }
+}

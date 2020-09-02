69c41d1d16b2175590fbd6138a6bb3914e48403f
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-09-02 11:36:23 +0100

    Add Get Job Summary Request and Response

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Request/GetJobSummaryRequest.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using HelpMyStreet.Contracts.RequestService.Response;
+
+namespace HelpMyStreet.Contracts.RequestService.Request
+{
+    public class GetJobSummaryRequest : IRequest<GetJobSummaryResponse>
+    {
+        public int JobID { get; set; }
+    }
+}

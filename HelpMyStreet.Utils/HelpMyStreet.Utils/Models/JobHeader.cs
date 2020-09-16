3cb50ecb2abf86d5b1fbe73d6a7b16adf3007a73
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-09-16 10:39:06 +0100

    Add JobHeader

---
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Models/JobHeader.cs
@@ -0,0 +1,20 @@
+﻿using HelpMyStreet.Utils.Enums;
+using System;
+
+namespace HelpMyStreet.Utils.Models
+{
+    public class JobHeader
+    {
+        public DateTime DateRequested { get; set; }
+        public DateTime DateStatusLastChanged { get; set; }
+        public int? ReferringGroupID { get; set; }
+        public double DistanceInMiles { get; set; }
+        public string PostCode { get; set; }
+        public bool IsHealthCritical { get; set; }
+        public DateTime DueDate { get; set; }
+        public SupportActivities SupportActivity { get; set; }
+        public JobStatuses JobStatus { get; set; }
+        public int JobID { get; set; }
+        public bool? Archive { get; set; }
+    }
+}

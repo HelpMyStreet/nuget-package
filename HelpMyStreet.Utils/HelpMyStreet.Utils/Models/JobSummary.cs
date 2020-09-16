9b663b0f281342c976bd8b175fa5316a750c649a
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-09-16 14:12:57 +0100

    Make JobSummary class extend JobHeader

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Models/JobSummary.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Models/JobSummary.cs
@@ -4,28 +4,15 @@ using System.Collections.Generic;
 
 namespace HelpMyStreet.Utils.Models
 {
-    public class JobSummary
+    public class JobSummary : JobHeader
     {
-        public int JobID { get; set; }
-        public JobStatuses JobStatus { get; set; }
         public int? VolunteerUserID { get; set; }
-        public SupportActivities SupportActivity { get; set; }
         public string Details { get; set; }
-        public DateTime DueDate { get; set; }
-        public bool IsHealthCritical { get; set; }
-        public string PostCode { get; set; }
-        public double DistanceInMiles { get; set; }
         public List<Question> Questions { get; set; }
-        public int? ReferringGroupID { get; set; }
         public List<int> Groups { get; set; }
         public string RecipientOrganisation { get; set; }
-        public DateTime DateStatusLastChanged { get; set; }
         public int DueDays { get; set; }
         public bool ConsentForContact { get; set; }
-        public DateTime DateRequested { get; set; }
         public RequestorType RequestorType { get; set; }
-        public bool? Archive { get; set; }
-
-
     }
 }

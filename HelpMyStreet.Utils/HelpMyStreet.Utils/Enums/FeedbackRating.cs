b197db7376dd99691a7de62960c9c525eee1303a
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-09-20 12:58:51 +0100

    add feedback rating and request roles

---
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Enums/FeedbackRating.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Contracts.RequestService.Response
+{
+    public enum FeedbackRating
+    {
+        HappyFace = 1,
+        SadFace = 2
+    }
+}

6b9c376d9fdb52ba82943f76f1f7aec334ef1fbb
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-09-24 15:17:25 +0100

    Add InterUserMessageRequest

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/CommunicationService/Request/EmailDetails.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Contracts.CommunicationService.Request
+{
+    public class EmailDetails
+    {
+        public string DisplayName { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}

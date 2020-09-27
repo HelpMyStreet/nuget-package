2370c3d8049d067ab61beac31293b165c435f9b3
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-09-27 21:15:05 +0100

    SimplifyInterUserMessage

--- HelpMyStreet.Utils/HelpMyStreet.Contracts/CommunicationService/Cosmos/SaveInterUserMessage.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/CommunicationService/Cosmos/SaveInterUserMessage.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using HelpMyStreet.Contracts.CommunicationService.Request;
+using System;
+using System.Collections.Generic;
 
 namespace HelpMyStreet.Contracts.CommunicationService.Cosmos
 {
     public class SaveInterUserMessage
     {
         public Guid ThreadId { get; set; }
-        public SaveMessageParticipant From { get; set; }
-        public SaveMessageParticipant To { get; set; }
+        public string SenderFirstName { get; set; }
         public int? JobId { get; set; }
         public string Content { get; set; }
+        public EmailDetails EmailDetails { get; set; }
+        public List<string> RecipientUserIds { get; set; }
     }
 }

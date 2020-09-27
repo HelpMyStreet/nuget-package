1351c67f209c45e1928dbcf78312220b9b6a8139
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-09-27 21:18:22 +0100

    Add RequestRoles

--- HelpMyStreet.Utils/HelpMyStreet.Contracts/CommunicationService/Cosmos/SaveInterUserMessage.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/CommunicationService/Cosmos/SaveInterUserMessage.cs
@@ -1,6 +1,8 @@
 ﻿using HelpMyStreet.Contracts.CommunicationService.Request;
+using HelpMyStreet.Utils.Enums;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace HelpMyStreet.Contracts.CommunicationService.Cosmos
 {
@@ -8,9 +10,11 @@ namespace HelpMyStreet.Contracts.CommunicationService.Cosmos
     {
         public Guid ThreadId { get; set; }
         public string SenderFirstName { get; set; }
+        public RequestRoles SenderRequestRoles { get; set; }
         public int? JobId { get; set; }
         public string Content { get; set; }
         public EmailDetails EmailDetails { get; set; }
         public List<string> RecipientUserIds { get; set; }
+        public RequestRoles RecipientRequestRoles { get; set; }
     }
 }

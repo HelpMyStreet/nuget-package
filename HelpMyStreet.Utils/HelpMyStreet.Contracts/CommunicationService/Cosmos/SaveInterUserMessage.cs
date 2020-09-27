eeca40ca752d608de3d9d58a7b7bcd2972feee0a
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-09-27 12:36:20 +0100

    AddCosmosModels

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/CommunicationService/Cosmos/SaveInterUserMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelpMyStreet.Contracts.CommunicationService.Cosmos
+{
+    public class SaveInterUserMessage
+    {
+        public Guid ThreadId { get; set; }
+        public SaveMessageParticipant From { get; set; }
+        public SaveMessageParticipant To { get; set; }
+        public int? JobId { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/CommunicationService/Cosmos/SaveMessageParticipant.cs
@@ -0,0 +1,12 @@
+﻿using HelpMyStreet.Contracts.CommunicationService.Request;
+
+namespace HelpMyStreet.Contracts.CommunicationService.Cosmos
+{
+    public class SaveMessageParticipant
+    {
+        public int? UserId { get; set; }
+        public EmailDetails EmailDetails { get; set; }
+        public SaveUsersFromGroups GroupRoleType { get; set; }
+        public RequestRoleType RequestRoleType { get; set; }
+    }
+}

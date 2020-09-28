f314227a7ebf210ced3605bb2ffbeb6e22f8d95c
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-09-28 08:30:36 +0100

    Add new method to GetGroupMembers

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Request/GetGroupMembersForGivenRoleRequest.cs
@@ -0,0 +1,12 @@
+﻿using HelpMyStreet.Contracts.GroupService.Response;
+using MediatR;
+
+namespace HelpMyStreet.Contracts.GroupService.Request
+{
+    public class GetGroupMembersForGivenRoleRequest : IRequest<GetGroupMembersForGivenRoleResponse>
+    {
+        public int GroupId { get; set; }
+        public int UserId { get; set; }
+        public int AuthorisingUserID { get; set; }
+    }
+}
---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Response/GetGroupMembersForGivenRoleResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Contracts.GroupService.Response
+{
+    public class GetGroupMembersForGivenRoleResponse
+    {
+        public List<int> UserIDs { get; set; }
+    }
+}

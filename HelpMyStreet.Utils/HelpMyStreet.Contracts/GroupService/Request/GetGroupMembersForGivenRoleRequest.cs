78c0c33fc9d848c42d8799bf1d5b5f7fb94ef74a
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-09-28 08:38:17 +0100

    Remove userID and replace with GroupRole

--- HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Request/GetGroupMembersForGivenRoleRequest.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Request/GetGroupMembersForGivenRoleRequest.cs
@@ -1,4 +1,5 @@
 ﻿using HelpMyStreet.Contracts.GroupService.Response;
+using HelpMyStreet.Utils.Enums;
 using MediatR;
 
 namespace HelpMyStreet.Contracts.GroupService.Request
@@ -6,7 +7,7 @@ namespace HelpMyStreet.Contracts.GroupService.Request
     public class GetGroupMembersForGivenRoleRequest : IRequest<GetGroupMembersForGivenRoleResponse>
     {
         public int GroupId { get; set; }
-        public int UserId { get; set; }
+        public RoleRequest GroupRole { get; set; }
         public int AuthorisingUserID { get; set; }
     }
 }

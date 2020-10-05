b9ad590905e98e3cdcdea61fabe3e31d7585cdf9
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-10-05 10:30:21 +0100

    Add CreateLink objects

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/CommunicationService/Request/CreateLinkRequest.cs
@@ -0,0 +1,11 @@
+﻿using HelpMyStreet.Contracts.CommunicationService.Response;
+using MediatR;
+
+namespace HelpMyStreet.Contracts.CommunicationService.Request
+{
+    public class CreateLinkRequest : IRequest<CreateLinkResponse>
+    {
+        public string LinkDestination { get; set; }
+        public int ExpiryDays { get; set; }
+    }
+}

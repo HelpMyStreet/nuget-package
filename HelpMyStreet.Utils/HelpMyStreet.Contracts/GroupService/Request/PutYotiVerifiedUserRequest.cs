dbb8c3564b7ce0eec7cf1192ff1788bb06c912f0
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-10-12 15:44:35 +0100

    Add yoti verified user

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Request/PutYotiVerifiedUserRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Contracts.GroupService.Request
+{
+    public class PutYotiVerifiedUserRequest : IRequest<bool>
+    {
+		public int UserId { get; set; }
+		public string Reference { get; set; }
+		public string Notes { get; set; }
+	}
+}

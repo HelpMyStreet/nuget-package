d2e436f810a0a70223528cb4202657b6474a5c0b
Mike Matthews <mike@factor-50.co.uk>
2020-10-21 10:14:50 +0100

    Moving extension methods from front end

---
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelpMyStreet.Utils.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToTitleCase(this string inputString)
+        {
+            return string.Join(" ", inputString.Split(' ').Where(word => word.Length > 0).Select(word => char.ToUpper(word[0]) + word.Substring(1).ToLower()));
+        }
+    }
+}

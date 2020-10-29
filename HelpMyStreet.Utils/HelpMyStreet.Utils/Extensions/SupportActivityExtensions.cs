ed1e466aaa616aebe87473a2e8326f43fd5cc9f3
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-10-29 10:00:58 +0000

    add friendly name for new activities

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Extensions/SupportActivityExtensions.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Extensions/SupportActivityExtensions.cs
@@ -23,6 +23,8 @@ namespace HelpMyStreet.Utils.Extensions
                 SupportActivities.Other => "your requested activity",
                 SupportActivities.CommunityConnector => "a community connector",
                 SupportActivities.MedicalAppointmentTransport => "medical appointment transport",
+                SupportActivities.ColdWeatherArmy => "providing help in emergency situations during a cold snap",
+                SupportActivities.Transport => "providing transport for essential appointments",
                 _ => throw new ArgumentException(message: $"Unexpected SupportActivity: {activity}", paramName: nameof(activity))
             };
         }
@@ -45,6 +47,8 @@ namespace HelpMyStreet.Utils.Extensions
                 SupportActivities.Other => "Other",
                 SupportActivities.CommunityConnector => "Community Connector",
                 SupportActivities.MedicalAppointmentTransport => "Medical Appointment Transport",
+                SupportActivities.ColdWeatherArmy => "Providing Help In Emergency Situations During A Cold Snap",
+                SupportActivities.Transport => "Providing Transport Essential Appointments",
                 _ => throw new ArgumentException(message: $"Unexpected SupportActivity: {activity}", paramName: nameof(activity))
             };
         }

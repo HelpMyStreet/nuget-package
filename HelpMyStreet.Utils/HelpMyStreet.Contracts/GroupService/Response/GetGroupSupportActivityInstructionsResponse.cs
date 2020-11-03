be47709a5694fd6757174edd8a2d38b3e62f2a4d
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-11-03 14:25:48 +0000

    Add new enum and method request/response classes

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Response/GetGroupSupportActivityInstructionsResponse.cs
@@ -0,0 +1,9 @@
+﻿using HelpMyStreet.Utils.Models;
+
+namespace HelpMyStreet.Contracts.GroupService.Response
+{
+    public class GetGroupSupportActivityInstructionsResponse
+    {
+        public Instructions Instructions { get; set; }
+    }
+}

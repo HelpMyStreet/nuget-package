823d2246b17404ccb681cb203b855f6159590676
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-11-25 12:01:53 +0000

    add GetGroupNewRequestNotificationStrategy

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Request/GetGroupNewRequestNotificationStrategyRequest.cs
@@ -0,0 +1,13 @@
+﻿using HelpMyStreet.Contracts.GroupService.Response;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace HelpMyStreet.Contracts.GroupService.Request
+{
+    public class GetGroupNewRequestNotificationStrategyRequest : IRequest<GetGroupNewRequestNotificationStrategyResponse>
+    {
+        [Required]
+        public int GroupID { get; set; }
+    }
+
+}
---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Response/GetGroupNewRequestNotificationStrategyResponse.cs
@@ -0,0 +1,13 @@
+﻿using HelpMyStreet.Utils.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Contracts.GroupService.Response
+{
+    public class GetGroupNewRequestNotificationStrategyResponse
+    {
+        public int MaxVolunteer { get; set; }
+        public NewRequestNotificationStrategy NewRequestNotificationStrategy { get; set; }
+    }
+}

da545a44ae239457b74e606400df634f30174482
Mike Matthews <mike@factor-50.co.uk>
2020-12-07 15:48:16 +0000

    Complete and Incomplete extensions for JobStatuses enum

--- HelpMyStreet.Utils/HelpMyStreet.UnitTests/JobStatusExtensionsTests.cs
+++ HelpMyStreet.Utils/HelpMyStreet.UnitTests/JobStatusExtensionsTests.cs
@@ -23,5 +23,29 @@ namespace HelpMyStreet.UnitTests
         {
             Assert.AreEqual("Done", JobStatuses.Done.FriendlyName());
         }
+
+        [Test]
+        public void Complete_Done()
+        {
+            Assert.AreEqual(true, JobStatuses.Done.Complete());
+        }
+
+        [Test]
+        public void Complete_InProgress()
+        {
+            Assert.AreEqual(false, JobStatuses.InProgress.Complete());
+        }
+
+        [Test]
+        public void InComplete_Cancelled()
+        {
+            Assert.AreEqual(false, JobStatuses.Cancelled.Incomplete());
+        }
+
+        [Test]
+        public void InComplete_Open()
+        {
+            Assert.AreEqual(true, JobStatuses.Open.Incomplete());
+        }
     }
 }
--- HelpMyStreet.Utils/HelpMyStreet.Utils/Extensions/JobStatusExtensions.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Extensions/JobStatusExtensions.cs
@@ -1,4 +1,5 @@
-﻿using HelpMyStreet.Utils.Enums;
+﻿using System;
+using HelpMyStreet.Utils.Enums;
 
 namespace HelpMyStreet.Utils.Extensions
 {
@@ -12,6 +13,24 @@ namespace HelpMyStreet.Utils.Extensions
                 JobStatuses.InProgress => "In Progress",
                 _ => status.ToString()
             };
+        }
+
+        public static bool Incomplete(this JobStatuses status)
+        {
+            return status switch
+            {
+                JobStatuses.New => true,
+                JobStatuses.Open => true,
+                JobStatuses.InProgress => true,
+                JobStatuses.Done => false,
+                JobStatuses.Cancelled => false,
+                _ => throw new ArgumentException($"Unexpected JobStatuses value {status}", nameof(status))
+            };
+        }
+
+        public static bool Complete(this JobStatuses status)
+        {
+            return !status.Incomplete();
         }
     }
 }

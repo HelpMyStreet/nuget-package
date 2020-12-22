9b2d76655a01452fdeeaa261cd4f551e656db480
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-12-22 08:45:14 +0000

    Add new post new request shift

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Request/PostNewRequestForHelpShiftRequest.cs
@@ -0,0 +1,22 @@
+﻿using HelpMyStreet.Contracts.RequestService.Response;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using HelpMyStreet.Utils.Models;
+using HelpMyStreet.Utils.Enums;
+
+namespace HelpMyStreet.Contracts.RequestService.Request
+{
+    public class PostNewRequestForHelpShiftRequest : IRequest<PostNewRequestForHelpShiftResponse>
+    {
+        public string OtherDetails { get; set; }        
+        public int CreatedByUserId { get; set; }        
+        public int ReferringGroupId { get; set; }
+        public string Source { get; set; }
+        public string LocationRef { get; set; }
+        public DateTime StartDate { get; set; }
+        public int ShiftLength { get; set; }
+        public Dictionary<SupportActivities, int> DictSupportActivities { get; set; }
+    }
+}
---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Response/PostNewRequestForHelpShiftResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Contracts.RequestService.Response
+{
+    public class PostNewRequestForHelpShiftResponse
+    {
+        public int RequestID { get; set; }
+    }
+}

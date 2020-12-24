9f66a4fd6d28728bd7d1136706375c50ade455b1
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-12-24 12:09:34 +0000

    GetUserShifts

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Request/GetUserShiftsRequest.cs
@@ -0,0 +1,19 @@
+﻿using HelpMyStreet.Utils.Enums;
+using HelpMyStreet.Utils.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using HelpMyStreet.Contracts.RequestService.Response;
+
+namespace HelpMyStreet.Contracts.RequestService.Request
+{
+    public class GetUserShiftsRequest : IRequest<GetUserShiftsResponse>
+    {
+        public int VolunteerUserId { get; set; }
+        public int AuthorisedByUserId { get; set; }
+        public JobStatusRequest JobStatusRequest { get; set; }
+        public DateTime? DateFrom { get; set; }
+        public DateTime? DateTo { get; set; }
+    }
+}

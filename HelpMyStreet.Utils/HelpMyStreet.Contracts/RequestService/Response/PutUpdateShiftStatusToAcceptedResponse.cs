6aa835d14998b0ad2108b77270fd109db4e98c94
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-12-24 12:21:09 +0000

    Add JobStatuses Accepted Add new method PutUpdateShiftStatusToAccepted

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Response/PutUpdateShiftStatusToAcceptedResponse.cs
@@ -0,0 +1,13 @@
+﻿using HelpMyStreet.Utils.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Contracts.RequestService.Response
+{
+    public class PutUpdateShiftStatusToAcceptedResponse
+    {
+        public int JobID { get; set; }
+        public UpdateJobStatusOutcome Outcome { get; set; }
+    }
+}

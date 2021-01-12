fad25267591ab3ce7197d72c25a7aaea8603e9bc
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-01-12 11:36:33 +0000

    Make volunteer userId nullable to match db table

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Models/ShiftJobSummary.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Models/ShiftJobSummary.cs
@@ -5,7 +5,7 @@ namespace HelpMyStreet.Utils.Models
     public class ShiftJobSummary
     {     
         public int ID { get; set; }
-        public int VolunteerUserID { get; set; }
+        public int? VolunteerUserID { get; set; }
         public SupportActivities Activity { get; set; }
         public JobStatuses JobStatuses { get; set; }
     }

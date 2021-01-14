54ce83d5b8dfa66adef2bfb305f68e2804b60a29
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-01-14 09:00:30 +0000

    Add GetNewShiftActions

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Request/GetNewShiftActionsRequest.cs
@@ -0,0 +1,11 @@
+﻿using HelpMyStreet.Contracts.GroupService.Response;
+using MediatR;
+
+namespace HelpMyStreet.Contracts.GroupService.Request
+{
+    public class GetNewShiftActionsRequest : IRequest<GetNewShiftActionsResponse>
+    {
+        public int ReferringGroupId { get; set; }
+        public string Source { get; set; }
+    }
+}
---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Response/GetNewShiftActionsResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelpMyStreet.Contracts.GroupService.Response
+{
+    public class GetNewShiftActionsResponse
+    {
+        public Dictionary<Guid,TaskAction> Actions { get; set; }
+    }
+}

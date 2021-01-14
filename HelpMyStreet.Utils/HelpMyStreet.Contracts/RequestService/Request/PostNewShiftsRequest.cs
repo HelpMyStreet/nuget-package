29b552bae553c8a3717b7ddc81d83590368ddb1b
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-01-14 08:50:31 +0000

    Add guid

--- HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Request/PostNewShiftsRequest.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Request/PostNewShiftsRequest.cs
@@ -2,14 +2,12 @@
 using MediatR;
 using System;
 using System.Collections.Generic;
-using System.Text;
-using HelpMyStreet.Utils.Models;
-using HelpMyStreet.Utils.Enums;
 
 namespace HelpMyStreet.Contracts.RequestService.Request
 {
     public class PostNewShiftsRequest : IRequest<PostNewShiftsResponse>
     {
+        public Guid Guid { get; set; }
         public string OtherDetails { get; set; }        
         public int CreatedByUserId { get; set; }        
         public int ReferringGroupId { get; set; }

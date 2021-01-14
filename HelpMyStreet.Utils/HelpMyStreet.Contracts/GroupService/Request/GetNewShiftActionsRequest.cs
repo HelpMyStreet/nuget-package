ce2cf81c5f693f4b7087f220b7aaa28c19330cde
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-01-14 10:45:32 +0000

    Add Guid column to GetNewShiftActionsRequest

--- HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Request/GetNewShiftActionsRequest.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Request/GetNewShiftActionsRequest.cs
@@ -1,10 +1,12 @@
 ﻿using HelpMyStreet.Contracts.GroupService.Response;
 using MediatR;
+using System;
 
 namespace HelpMyStreet.Contracts.GroupService.Request
 {
     public class GetNewShiftActionsRequest : IRequest<GetNewShiftActionsResponse>
     {
+        public Guid Guid { get; set; }
         public int ReferringGroupId { get; set; }
         public string Source { get; set; }
     }

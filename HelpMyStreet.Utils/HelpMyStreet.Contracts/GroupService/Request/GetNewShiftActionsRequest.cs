bdcfdb89f632c4a586e42ce02ec6685e7fec59b5
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-01-14 11:10:20 +0000

    remove redundant Guid id

--- HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Request/GetNewShiftActionsRequest.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Request/GetNewShiftActionsRequest.cs
@@ -5,8 +5,7 @@ using System;
 namespace HelpMyStreet.Contracts.GroupService.Request
 {
     public class GetNewShiftActionsRequest : IRequest<GetNewShiftActionsResponse>
-    {
-        public Guid Guid { get; set; }
+    {        
         public int ReferringGroupId { get; set; }
         public string Source { get; set; }
     }

d69b3389e83c7234f2529891f25f1c49bd22168c
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-01-19 10:31:30 +0000

    add requestid and requesttype to getjobsummaryresponse

--- HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Response/GetJobSummaryResponse.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Response/GetJobSummaryResponse.cs
@@ -1,9 +1,12 @@
-﻿using HelpMyStreet.Utils.Models;
+﻿using HelpMyStreet.Utils.Enums;
+using HelpMyStreet.Utils.Models;
 
 namespace HelpMyStreet.Contracts.RequestService.Response
 {
     public class GetJobSummaryResponse
     {
         public JobSummary JobSummary { get; set; }
+        public int RequestID { get; set; }
+        public RequestType RequestType { get; set; }
     }
 }

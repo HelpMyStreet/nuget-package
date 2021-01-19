52341060fc40e8206146a180980e2696a9c722ee
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-01-19 21:04:38 +0000

    add longitude and latitude to locationDetails

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Models/LocationDetails.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Models/LocationDetails.cs
@@ -11,6 +11,8 @@ namespace HelpMyStreet.Utils.Models
         public string Name { get; set; }
         public string ShortName { get; set; }
         public Address Address { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
         public LocationInstructions LocationInstructions { get; set; }
     }
 }

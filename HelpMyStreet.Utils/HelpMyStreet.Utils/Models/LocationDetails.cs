31f4c1ef880e2340c7547ab2f7a7352a3d30d3b8
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-01-19 21:19:56 +0000

    change datatype to double

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Models/LocationDetails.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Models/LocationDetails.cs
@@ -11,8 +11,8 @@ namespace HelpMyStreet.Utils.Models
         public string Name { get; set; }
         public string ShortName { get; set; }
         public Address Address { get; set; }
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
         public LocationInstructions LocationInstructions { get; set; }
     }
 }

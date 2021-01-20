0e66b025862d49d320e69ab895b9c30e31beeef7
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-01-20 08:42:11 +0000

    Add GetLocationsByDistanceRequest and GetLocationsByDistanceResponse

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/AddressService/Response/LocationDistance.cs
@@ -0,0 +1,13 @@
+﻿using HelpMyStreet.Utils.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Contracts.AddressService.Response
+{
+    public class LocationDistance
+    {
+        public Location Location { get; set; }
+        public double DistanceFromPostCode { get; set; }
+    }
+}

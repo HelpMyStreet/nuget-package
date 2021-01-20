9f801b60964cddfe231975ca37b055665583e818
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-01-20 13:16:27 +0000

    add IRequest response

--- HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Request/PutUpdateRequestStatusToCancelledRequest.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Request/PutUpdateRequestStatusToCancelledRequest.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using HelpMyStreet.Contracts.RequestService.Response;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HelpMyStreet.Contracts.RequestService.Request
 {
-    public class PutUpdateRequestStatusToCancelledRequest
+    public class PutUpdateRequestStatusToCancelledRequest : IRequest<PutUpdateRequestStatusToCancelledResponse>
     {
         public int RequestID { get; set; }
         public int CreatedByUserID { get; set; }

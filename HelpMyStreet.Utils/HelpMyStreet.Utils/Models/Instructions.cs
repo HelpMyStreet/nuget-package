b530e613c1e39b3365ff83e435be9a4a1c1176b4
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-01-26 10:37:39 +0000

    add ActivityDetails Add GetLocation

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Models/Instructions.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Models/Instructions.cs
@@ -6,6 +6,7 @@ namespace HelpMyStreet.Utils.Models
     public class Instructions
     {
         public SupportActivityInstructions SupportActivityInstructions { get; set; }
+        public string ActivityDetails { get; set; }
         public string Intro { get; set; }
         public List<Step> Steps { get; set; }
         public string Close { get; set; }

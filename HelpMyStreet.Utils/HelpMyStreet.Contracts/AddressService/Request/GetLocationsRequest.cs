287417c00e0f7ebf18a4515141d83fcac2f70fcc
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-02-04 15:02:58 +0000

    Add GetLocations method

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/AddressService/Request/GetLocationsRequest.cs
@@ -0,0 +1,12 @@
+﻿using HelpMyStreet.Contracts.AddressService.Response;
+using MediatR;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace HelpMyStreet.Contracts.AddressService.Request
+{
+    public class GetLocationsRequest : IRequest<GetLocationsResponse>
+    {
+        public List<LocationRequest> LocationRequests { get; set; }
+    }
+}

cc4de4a39537f26f657f756f1ea58c5e272de897
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-02-04 14:35:58 +0000

    make AdditionalDataSource optional

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Models/Question.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Models/Question.cs
@@ -16,7 +16,7 @@ namespace HelpMyStreet.Utils.Models
         public string Answer { get; set; }
         public List<AdditonalQuestionData> AddtitonalData { get; set; }
         public bool AnswerContainsSensitiveData { get; set; }
-        public AdditionalDataSource AdditionalDataSource { get; set; }
+        public AdditionalDataSource? AdditionalDataSource { get; set; }
     }
 
 

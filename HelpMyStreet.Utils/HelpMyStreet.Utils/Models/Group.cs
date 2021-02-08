ef487e4a70477775a77a022f4f14466cc96f9fe7
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-02-08 14:37:11 +0000

    add new group fields

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Models/Group.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Models/Group.cs
@@ -11,5 +11,7 @@ namespace HelpMyStreet.Utils.Models
         public string GroupKey { get; set; }
         public int? ParentGroupId { get; set; }
         public bool ShiftsEnabled { get; set; }
+        public bool TasksEnabled { get; set; }
+        public bool HomepageEnabled { get; set; }
     }
 }

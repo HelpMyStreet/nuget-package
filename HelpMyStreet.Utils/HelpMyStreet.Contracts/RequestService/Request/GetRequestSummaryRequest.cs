42e08c5ddd41b4ed5c68ce340fa0fbb32aba0a11
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-02-15 08:57:47 +0000

    Remove  authorised by user id parameter from GetRequestSummary

--- HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Request/GetRequestSummaryRequest.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Request/GetRequestSummaryRequest.cs
@@ -5,7 +5,6 @@ namespace HelpMyStreet.Contracts.RequestService.Request
 {
     public class GetRequestSummaryRequest : IRequest<GetRequestSummaryResponse>
     {
-        public int AuthorisedByUserID { get; set; }
         public int RequestID { get; set; }
     }
 }

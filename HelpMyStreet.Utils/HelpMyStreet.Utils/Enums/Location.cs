673220badd2f1d8c9555479a6af63ccc39fcf661
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-02-23 11:10:13 +0000

    make locationid negative

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Enums/Location.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Enums/Location.cs
@@ -2,15 +2,15 @@
 {
     public enum Location
     {
-        LincolnCountyHospital = 1,
-        PilgramHospitalBolton = 2,
-        LouthCommunityHospital = 3,
-        TableTennisClubGrantham = 4,
-        WaddingtonBranchSurgerySouthLincoln = 5,
-        StMarysMedicalPracticeStamford = 6,
-        FranklinHallSpilsby = 7,
-        SidingsMedicalPracticeBoston = 8,
-        RustonsSportsAndSocialClubLincoln = 9,
-        PortlandMedicalPracticeLincoln = 10
+        LincolnCountyHospital = -1,
+        PilgramHospitalBolton = -2,
+        LouthCommunityHospital = -3,
+        TableTennisClubGrantham = -4,
+        WaddingtonBranchSurgerySouthLincoln = -5,
+        StMarysMedicalPracticeStamford = -6,
+        FranklinHallSpilsby = -7,
+        SidingsMedicalPracticeBoston = -8,
+        RustonsSportsAndSocialClubLincoln = -9,
+        PortlandMedicalPracticeLincoln = -10
     }
 }

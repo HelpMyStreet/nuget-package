2876107db2d033ec0ae8cc2aaaf4b5b49c218d59
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-02-24 10:46:15 +0000

    Tweak RequestSummary

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Models/RequestSummary.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Models/RequestSummary.cs
@@ -1,6 +1,7 @@
 ﻿using HelpMyStreet.Utils.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace HelpMyStreet.Utils.Models
@@ -8,7 +9,16 @@ namespace HelpMyStreet.Utils.Models
     public class RequestSummary
     {
         public Shift Shift { get; set; }
-        public List<JobBasic> JobSummaries { get; set; }
+        public List<JobSummary> JobSummaries { get; set; }
+        public List<ShiftJob> ShiftJobs { get; set; }
+        public List<JobBasic> JobBasics
+        {
+            get
+            {
+                return JobSummaries.Cast<JobBasic>()
+                    .Concat(ShiftJobs.Cast<JobBasic>()).ToList();                    
+            }
+        }
         public int RequestID { get; set; }
         public RequestType RequestType { get; set; }
         public int ReferringGroupID { get; set; }

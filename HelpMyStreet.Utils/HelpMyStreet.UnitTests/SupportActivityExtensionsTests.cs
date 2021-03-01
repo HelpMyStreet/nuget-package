d0bd41e5a01e761a93cab8cc0f80491f2096588f
Mike Matthews <mike@factor-50.co.uk>
2021-03-01 15:37:27 +0000

    Adding unit tests for SupportActivityExtensions, to ensure coverage

---
+++ HelpMyStreet.Utils/HelpMyStreet.UnitTests/SupportActivityExtensionsTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using HelpMyStreet.Utils.Enums;
+using HelpMyStreet.Utils.Extensions;
+using NUnit.Framework;
+
+namespace HelpMyStreet.UnitTests
+{
+    class SupportActivityExtensionsTests
+    {
+        [Test]
+        public void FriendlyNameForEmail_AllValuesCovered()
+        {
+            foreach (var val in Enum.GetValues(typeof(SupportActivities)))
+            {
+                _ = ((SupportActivities)val).FriendlyNameForEmail();
+            }
+        }
+
+        [Test]
+        public void FriendlyNameShort_AllValuesCovered()
+        {
+            foreach (var val in Enum.GetValues(typeof(SupportActivities)))
+            {
+                _ = ((SupportActivities)val).FriendlyNameShort();
+            }
+        }
+
+        [Test]
+        public void PerfectTense_Singular_AllValuesCovered()
+        {
+            foreach (var val in Enum.GetValues(typeof(SupportActivities)))
+            {
+                string s = ((SupportActivities)val).PerfectTense(1);
+                Assert.AreEqual(true, s.Contains("1"));
+            }
+        }
+
+        [Test]
+        public void PerfectTense_Plural_AllValuesCovered()
+        {
+            foreach (var val in Enum.GetValues(typeof(SupportActivities)))
+            {
+                string s = ((SupportActivities)val).PerfectTense(2);
+                Assert.AreEqual(true, s.Contains("2"));
+            }
+        }
+    }
+}

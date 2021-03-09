b2f69986e7722d78b6112542a0730df3bc740156
Mike Matthews <mike@factor-50.co.uk>
2021-03-09 11:33:35 +0000

    Adding Frequency enum

---
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Enums/Frequency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Utils.Enums
+{
+    public enum Frequency
+    {
+        Once = 10,
+        Daily = 20,
+        Weekly = 30,
+        Fortnightly = 40,
+        Monthly = 50,
+    }
+}

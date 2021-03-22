1fc5b0c6a6d7d5bb3d38e69a9171c67844655b4a
Mike Matthews <mike@factor-50.co.uk>
2021-03-22 16:33:54 +0000

    Adding Frequency extension methods

---
+++ HelpMyStreet.Utils/HelpMyStreet.UnitTests/FrequencyExtensionTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HelpMyStreet.Utils.Enums;
+using HelpMyStreet.Utils.Extensions;
+using NUnit.Framework;
+
+namespace HelpMyStreet.UnitTests
+{
+    class FrequencyExtensionTests
+    {
+        [Test]
+        public void FrequencyDays_AllValuesCovered()
+        {
+            foreach (Frequency val in Enum.GetValues(typeof(Frequency)))
+            {
+                if (val.Equals(Frequency.Once))
+                {
+                    // .FrequencyDays() intentionally not defined 
+                }
+                else
+                {
+                    _ = val.FrequencyDays();
+                }
+            }
+        }
+
+        [Test]
+        public void MaxOccurrences_AllValuesCovered()
+        {
+            foreach (Frequency val in Enum.GetValues(typeof(Frequency)))
+            {
+                _ = val.MaxOccurrences();
+            }
+        }
+    }
+}
--- HelpMyStreet.Utils/HelpMyStreet.Utils/Extensions/FrequencyExtensions.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Extensions/FrequencyExtensions.cs
@@ -16,6 +16,29 @@ namespace HelpMyStreet.Utils.Extensions
                 Frequency.EveryFourWeeks => 28,
                 _ => throw new ArgumentException(message: $"Unexpected Frequency: {frequency}", paramName: nameof(frequency))
             };
-        }   
+        }
+
+        public static int MaxOccurrences(this Frequency frequency)
+        {
+            return frequency switch
+            {
+                Frequency.Once => 1,
+                Frequency.Daily => 14,
+                Frequency.Weekly => 12,
+                Frequency.Fortnightly => 6,
+                Frequency.EveryFourWeeks => 3,
+                _ => throw new ArgumentException($"Unexpected Frequency {frequency}", nameof(frequency))
+            };
+        }
+
+        public static string FriendlyName(this Frequency frequency)
+        {
+            return frequency switch
+            {
+                Frequency.Once => "Just once",
+                Frequency.EveryFourWeeks => "Every 4 weeks",
+                _ => frequency.ToString()
+            };
+        }
     }
 }

b6ae14d4df30e22fa6fc21aff2a086d07059f3ed
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-03-23 08:28:58 +0000

    add RequestHelpFormVariantExtensions

---
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Extensions/RequestHelpFormVariantExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using HelpMyStreet.Utils.Enums;
+
+namespace HelpMyStreet.Utils.Extensions
+{
+    public static class RequestHelpFormVariantExtensions
+    {
+        public static bool IsPublic(this RequestHelpFormVariant requestHelpFormVariant)
+        {
+            return requestHelpFormVariant switch
+            {
+                RequestHelpFormVariant.AgeUKNorthWestKent_RequestSubmitter => false,
+                RequestHelpFormVariant.AgeUKFavershamAndSittingbourne_RequestSubmitter => false,
+                RequestHelpFormVariant.AgeUKSouthKentCoast_RequestSubmitter => false,
+                RequestHelpFormVariant.HLP_CommunityConnector => false,
+                RequestHelpFormVariant.AgeConnectsCardiff_RequestSubmitter=> false,
+                RequestHelpFormVariant.MeadowsCommunityHelpers_RequestSubmitter => false,
+                _ => true
+            };
+        }
+    }
+}

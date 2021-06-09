e99674004e40837b5b6a1347b5264c707247e01b
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-06-09 10:59:16 +0100

    Add UserIsYotiVerified

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Models/UserInGroup.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Models/UserInGroup.cs
@@ -7,11 +7,19 @@ namespace HelpMyStreet.Utils.Models
 {
     public class UserInGroup
     {
+        private const int YOTI_CREDENTIAL_ID = -1;
         public int UserId { get; set; }
         public int GroupId { get; set; }
         public List<GroupRoles> GroupRoles { get; set; }
         public List<int> ValidCredentials { get; set; }
         public List<UserRoleAudit> UserRoleAudit { get; set; }
+        public bool UserIsYotiVerified
+        {
+            get
+            {
+                return ValidCredentials.Contains(YOTI_CREDENTIAL_ID);
+            }
+        }
     }
 
 }

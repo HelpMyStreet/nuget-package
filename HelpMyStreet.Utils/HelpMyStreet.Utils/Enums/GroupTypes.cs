1b302a2e5993081a1f8b183dd0398c849649b577
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-06-14 10:47:06 +0100

    Add GroupTypes

---
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Enums/GroupTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Utils.Enums
+{
+    public enum GroupTypes
+    {
+        Local = 0,
+        Regional = 1,
+        National = 2
+    }
+}

536e1af043f890a4214490c9dd5607e29eb4a7e4
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-06-14 13:51:36 +0100

    Add GroupType property

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Models/Group.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Models/Group.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HelpMyStreet.Utils.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -14,5 +15,6 @@ namespace HelpMyStreet.Utils.Models
         public bool TasksEnabled { get; set; }
         public bool HomepageEnabled { get; set; }
         public string GeographicName { get; set; }
+        public GroupTypes GroupType { get; set; }
     }
 }

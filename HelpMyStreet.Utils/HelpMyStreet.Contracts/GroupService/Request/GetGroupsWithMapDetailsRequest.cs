19eb385fb804ebbdb5c181e9363b23779781a733
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-06-17 11:58:48 +0100

    Add GetGroupsWithMapDetails

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Request/GetGroupsWithMapDetailsRequest.cs
@@ -0,0 +1,13 @@
+﻿using HelpMyStreet.Contracts.GroupService.Response;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace HelpMyStreet.Contracts.GroupService.Request
+{
+    public class GetGroupsWithMapDetailsRequest : IRequest<GetGroupsWithMapDetailsResponse>
+    {
+        [Required]
+        public MapLocationRequest MapLocation { get; set; }
+    }
+
+}

f0b50a600d73b3d56b2d4a6daba3505b17f7e7fe
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-07-01 13:48:09 +0100

    introduce HLP

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Enums/Groups.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Enums/Groups.cs
@@ -5,6 +5,7 @@ namespace HelpMyStreet.Utils.Enums
         Generic = -1,
         FTLOS = -2,
         AgeUKLSL = -3,
+        HLP = -4,
         Tankersley = -5,
         Ruddington = -6,
         AgeUKWirral = -7,

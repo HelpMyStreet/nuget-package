82f2edc39c6ffb361ed102360909127b30b8d924
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-07-01 11:05:56 +0100

    remove hlp

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Enums/Groups.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Enums/Groups.cs
@@ -5,7 +5,6 @@ namespace HelpMyStreet.Utils.Enums
         Generic = -1,
         FTLOS = -2,
         AgeUKLSL = -3,
-        HLP = -4,
         Tankersley = -5,
         Ruddington = -6,
         AgeUKWirral = -7,

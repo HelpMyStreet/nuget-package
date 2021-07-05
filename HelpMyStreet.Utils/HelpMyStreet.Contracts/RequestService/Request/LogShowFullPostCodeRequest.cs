849c23d042327786da5d9b6c128fd18589b0a5a5
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-07-05 10:31:12 +0100

    Add LogShowFullPostCode

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Request/LogShowFullPostCodeRequest.cs
@@ -0,0 +1,15 @@
+﻿using HelpMyStreet.Contracts.RequestService.Response;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Contracts.RequestService.Request
+{
+    public class LogShowFullPostCodeRequest : IRequest<LogShowFullPostCodeResponse>
+    {
+        public int UserID { get; set; }
+        public int JobID { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}

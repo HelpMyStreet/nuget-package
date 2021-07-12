34e8d931a32024fd2a08dd9172fdd9d6416e09fe
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-07-12 10:00:11 +0100

    Add GetUserLocations

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Response/GetUserLocationsResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace HelpMyStreet.Contracts.GroupService.Response
+{
+    public class GetUserLocationsResponse
+    {
+        public List<int> Locations { get; set; }
+    }
+}

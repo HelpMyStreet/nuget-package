a549efdd5643635033b662f3b65c302a2f0230b4
Geoff Burnhill <geoff@factor-50.co.uk>
2021-07-12 15:32:26 +0100

    Add DistanceBetweenPostcode Contracts

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/AddressService/Request/GetDistanceBetweenPostcodesRequest.cs
@@ -0,0 +1,12 @@
+﻿using HelpMyStreet.Contracts.AddressService.Response;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace HelpMyStreet.Contracts.AddressService.Request
+{
+    public class GetLocationBetweenPostcodesRequest
+    {
+        public string Postcode1  { get; set; }
+        public string Postcode2 { get; set; }
+    }
+}
---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/AddressService/Response/GetDistanceBetweenPostcodesResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace HelpMyStreet.Contracts.AddressService.Response
+{
+    [DataContract(Name = "GetDistanceBetweenPostcodesResponse")]
+    public class GetDistanceBetweenPostcodesResponse
+    {
+        [DataMember(Name = "distanceInMiles")]
+        public double DistanceInMiles { get; set; }
+
+    }
+}

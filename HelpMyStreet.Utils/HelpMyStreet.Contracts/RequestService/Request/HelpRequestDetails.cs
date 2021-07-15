968077cafa375dbc8e00241a15193bda54e41f89
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-07-15 11:28:13 +0100

    Add PostRequestForHelp

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Request/HelpRequestDetails.cs
@@ -0,0 +1,13 @@
+﻿using HelpMyStreet.Utils.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Contracts.RequestService.Request
+{
+    public class HelpRequestDetail
+    {
+        public HelpRequest HelpRequest { get; set; }
+        public NewJobsRequest NewJobsRequest { get; set; }
+    }
+}

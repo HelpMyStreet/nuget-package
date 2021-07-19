8b024dac4c7281508fad9e7a0042b88b62bdabf0
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-07-19 15:42:45 +0100

    Add repeat and multi flags to HelpRequestDetails

--- HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Request/HelpRequestDetails.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Request/HelpRequestDetails.cs
@@ -1,6 +1,8 @@
-﻿using HelpMyStreet.Utils.Models;
+﻿using HelpMyStreet.Utils.Enums;
+using HelpMyStreet.Utils.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace HelpMyStreet.Contracts.RequestService.Request
@@ -9,5 +11,33 @@ namespace HelpMyStreet.Contracts.RequestService.Request
     {
         public HelpRequest HelpRequest { get; set; }
         public NewJobsRequest NewJobsRequest { get; set; }
+
+        public bool Repeat 
+        { 
+            get
+            {
+                return NewJobsRequest.Jobs.First().NumberOfRepeats > 1;
+            }
+        }
+
+        public bool MultiVolunteer
+        {
+            get
+            {
+                bool multi = false;
+                var numberOfSlotsQuestion = NewJobsRequest.Jobs.First().Questions?.Where(x => x.Id == (int)Questions.NumberOfSlots).FirstOrDefault();
+
+                if (numberOfSlotsQuestion != null)
+                {
+                    int numberOfSlots = Convert.ToInt32(numberOfSlotsQuestion.Answer);
+
+                    multi = numberOfSlots > 1;
+                }
+                return multi;
+                   
+            }
+        }
+
+
     }
 }

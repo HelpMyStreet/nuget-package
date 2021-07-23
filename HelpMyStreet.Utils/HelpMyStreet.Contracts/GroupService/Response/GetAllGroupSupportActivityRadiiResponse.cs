e6357d3caa11690176fca2b32c7d9374f9ee9cc1
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-07-23 23:02:47 +0100

    Add all GetAllGroupSupportActivityRadii

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Response/GetAllGroupSupportActivityRadiiResponse.cs
@@ -0,0 +1,10 @@
+﻿using HelpMyStreet.Utils.Models;
+using System.Collections.Generic;
+
+namespace HelpMyStreet.Contracts.GroupService.Response
+{
+    public class GetAllGroupSupportActivityRadiiResponse
+    {
+        public List<GroupSupportActivityRadius> GroupSupportActivityRadii { get; set; }
+    }
+}

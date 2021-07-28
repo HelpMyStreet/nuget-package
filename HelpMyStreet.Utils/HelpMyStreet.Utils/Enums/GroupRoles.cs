a8d5030761e2463ef3d7f8d278de80315942e222
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-07-28 09:31:55 +0100

    add properties  to GroupRoles enum

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Enums/GroupRoles.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Enums/GroupRoles.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace HelpMyStreet.Utils.Enums
 {
     public enum GroupRoles
     {
+        [Description("Standard")]
         Member = 1,
+        
+        [Description("Admin")]
         TaskAdmin = 2,
+        
+        [Description("Admin")]
         UserAdmin = 3,
+
+        [Description("Admin")]
         Owner = 4,
+
+        [Description("Admin")]
         RequestSubmitter = 5,
+
+        [Description("Standard")]
         Volunteer = 6,
+
+        [Description("Admin")]
         UserAdmin_ReadOnly = 7
     }
 }

eae04dc8469f9c16367800183d4f994823e4b8e7
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-08-10 08:53:47 +0100

    Add PutUpdateJobQuestion Request and Response Object

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Request/PutUpdateJobQuestionRequest.cs
@@ -0,0 +1,16 @@
+﻿using HelpMyStreet.Contracts.RequestService.Response;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Contracts.RequestService.Request
+{
+    public class PutUpdateJobQuestionRequest : IRequest<PutUpdateJobQuestionResponse>
+    {
+        public int JobID { get; set; }
+        public int QuestionID { get; set; }
+        public string Answer { get; set; }
+        public int AuthorisedByUserID { get; set; }
+    }
+}

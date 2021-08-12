6ca3e4b4587d60d00e09cbab0e176f5750480e6c
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-08-12 13:00:13 +0100

    Add ToHtmlSafeStringWithLineBreaks

---
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Helpers/StringHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace HelpMyStreet.Utils.Helpers
+{
+    public static class StringHelpers
+    {
+        private static readonly Regex LineBreakRegex = new Regex(@"(\n|\r){1,2}");
+
+        public static string ToHtmlSafeStringWithLineBreaks(this string inputString)
+        {
+            return LineBreakRegex.Replace(HttpUtility.HtmlEncode(inputString), "<br />");
+        }
+    }
+}

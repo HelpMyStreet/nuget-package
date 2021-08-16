d6a25d5816974ec3e0ad2f102e24c8c4d6d8b6c1
Mike Matthews <mike@factor-50.co.uk>
2021-08-16 12:01:07 +0100

    FE-1170 Allowing override of WhenDataIsStaleDelegate

--- HelpMyStreet.Utils/HelpMyStreet.Cache/IMemDistCacheFactory.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Cache/IMemDistCacheFactory.cs
@@ -8,8 +8,8 @@ namespace HelpMyStreet.Cache
         /// Get cache
         /// </summary>
         /// <param name="howLongToKeepStaleData">How long to keep stale data in the cache</param>
-        /// <param name="whenDataIsStaleDelegate">When the data should be considered stale</param> 
+        /// <param name="defaultWhenDataIsStaleDelegate">When the data should be considered stale (can be overriden when adding an item to the cache)</param> 
         /// <returns></returns>
-        IMemDistCache<T> GetCache(TimeSpan howLongToKeepStaleData, Func<DateTimeOffset, DateTimeOffset> whenDataIsStaleDelegate);
+        IMemDistCache<T> GetCache(TimeSpan howLongToKeepStaleData, Func<DateTimeOffset, DateTimeOffset> defaultWhenDataIsStaleDelegate);
     }
 }

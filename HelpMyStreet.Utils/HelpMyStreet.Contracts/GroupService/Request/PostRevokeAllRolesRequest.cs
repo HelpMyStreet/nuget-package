eb91e855409a595bdd4ba5407540dcd51d611d32
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-10-21 10:04:49 +0100

    Add PostRevokeAllRoles

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Request/PostRevokeAllRolesRequest.cs
@@ -0,0 +1,18 @@
+﻿using HelpMyStreet.Contracts.GroupService.Response;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace HelpMyStreet.Contracts.GroupService.Request
+{
+    public class PostRevokeAllRolesRequest : IRequest<PostRevokeRoleResponse>
+    {
+        [Required]
+        public int? GroupID { get; set; }
+
+        [Required]
+        public int? UserID { get; set; }
+
+        [Required]
+        public int? AuthorisedByUserID { get; set; }
+    }
+}
---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Response/PostRevokeAllRolesResponse.cs
@@ -0,0 +1,10 @@
+﻿using HelpMyStreet.Utils.Enums;
+using MediatR;
+
+namespace HelpMyStreet.Contracts.GroupService.Response
+{
+    public class PostRevokeAllRolesResponse
+    {
+        public GroupPermissionOutcome Outcome { get; set; }
+    }
+}

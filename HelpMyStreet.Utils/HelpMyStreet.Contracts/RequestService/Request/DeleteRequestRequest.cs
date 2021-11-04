f0a05b3cdffa841057000f48def5763f28b5f379
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-11-04 12:40:20 +0000

    Add DeleteRequest method

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Request/DeleteRequestRequest.cs
@@ -0,0 +1,18 @@
+﻿using HelpMyStreet.Contracts.RequestService.Response;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace HelpMyStreet.Contracts.RequestService.Request
+{
+    public class DeleteRequestRequest : IRequest<DeleteRequestResponse>
+    {
+        [Required]
+        public int RequestID { get; set; }
+
+        [Required]
+        public string Postcode { get; set; }
+    }
+}

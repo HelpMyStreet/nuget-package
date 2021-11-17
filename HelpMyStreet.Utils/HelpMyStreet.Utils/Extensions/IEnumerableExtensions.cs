b839667f451059d6cbd7edc030ca720a33b0d8b3
Mike Matthews <mike@factor-50.co.uk>
2021-11-17 15:01:06 +0000

    Use IEnumerable rather than List specifically

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Extensions/IEnumerableExtensions.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Extensions/IEnumerableExtensions.cs
@@ -3,15 +3,14 @@ using System.Linq;
 
 namespace HelpMyStreet.Utils.Extensions
 {
-    public static class ListExtensions
+    public static class IEnumerableExtensions
     {
-        public static List<List<T>> ChunkBy<T>(this List<T> source, int chunkSize)
+        public static IEnumerable<IEnumerable<T>> ChunkBy<T>(this IEnumerable<T> source, int chunkSize)
         {
             return source
                 .Select((x, i) => new { Index = i, Value = x })
                 .GroupBy(x => x.Index / chunkSize)
-                .Select(x => x.Select(v => v.Value).ToList())
-                .ToList();
+                .Select(x => x.Select(v => v.Value));
         }
     }
 }

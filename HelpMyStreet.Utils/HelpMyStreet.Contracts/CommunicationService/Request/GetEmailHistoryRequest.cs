c9873d5940f9205e259ba993b428e0d4ed550893
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-11-23 15:20:58 +0000

    Add EmailHistory

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/CommunicationService/Request/GetEmailHistoryRequest.cs
@@ -0,0 +1,10 @@
+﻿using HelpMyStreet.Contracts.CommunicationService.Response;
+using MediatR;
+
+namespace HelpMyStreet.Contracts.CommunicationService.Request
+{
+    public class GetEmailHistoryRequest : IRequest<GetEmailHistoryResponse>
+    {
+        public int RequestId { get; set; }
+    }
+}
---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/CommunicationService/Response/GetEmailHistoryResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace HelpMyStreet.Contracts.CommunicationService.Response
+{
+    public class GetEmailHistoryResponse
+    {
+        public List<EmailHistory> EmailHistory { get; set; }
+    }
+}

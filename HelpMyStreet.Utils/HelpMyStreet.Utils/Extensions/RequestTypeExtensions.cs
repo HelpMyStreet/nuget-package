861121886a3f9d0853eac08090bf9d9f04a234c8
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-12-08 10:15:21 +0000

    Add PerfectTense for RequestType

---
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Extensions/RequestTypeExtensions.cs
@@ -0,0 +1,30 @@
+﻿using HelpMyStreet.Utils.Enums;
+using System;
+
+namespace HelpMyStreet.Utils.Extensions
+{
+    public static class RequestTypeExtensions
+    {
+        public static string PerfectTense(this RequestType requestType, int count)
+        {
+            if (count <= 1)
+            {
+                return requestType switch
+                {
+                    RequestType.Task => "task",
+                    RequestType.Shift => "shift",
+                    _ => throw new ArgumentException(message: $"Unexpected RequestType: {requestType}")
+                };
+            }
+            else
+            {
+                return requestType switch
+                {
+                    RequestType.Task => "tasks",
+                    RequestType.Shift => "shifts",
+                    _ => throw new ArgumentException(message: $"Unexpected RequestType: {requestType}")
+                };
+            }
+        }
+    }
+}

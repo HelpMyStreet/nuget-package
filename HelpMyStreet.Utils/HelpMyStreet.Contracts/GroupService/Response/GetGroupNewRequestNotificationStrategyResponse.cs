92c4360121e71528fd3e0b0ff1a30dc5058add87
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2022-01-11 09:45:10 +0000

    Remove UrgentRequestNotificationStrategy

--- HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Response/GetGroupNewRequestNotificationStrategyResponse.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Response/GetGroupNewRequestNotificationStrategyResponse.cs
@@ -1,7 +1,4 @@
 ﻿using HelpMyStreet.Utils.Enums;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace HelpMyStreet.Contracts.GroupService.Response
 {
@@ -9,6 +6,5 @@ namespace HelpMyStreet.Contracts.GroupService.Response
     {
         public int MaxVolunteer { get; set; }
         public NewRequestNotificationStrategy NewRequestNotificationStrategy { get; set; }
-        public UrgentRequestNotificationStrategy? UrgentRequestNotificationStrategy { get; set; }
     }
 }

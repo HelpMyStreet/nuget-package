a01905bbf280b870d4ce80b8b44929a9cca4f281
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2022-02-09 08:43:07 +0000

    Add GetNewRequestActionsSimplified

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Request/GetNewRequestActionsSimplifiedRequest.cs
@@ -0,0 +1,13 @@
+﻿using HelpMyStreet.Contracts.GroupService.Response;
+using HelpMyStreet.Contracts.RequestService.Request;
+using MediatR;
+
+namespace HelpMyStreet.Contracts.GroupService.Request
+{
+    public class GetNewRequestActionsSimplifiedRequest : IRequest<GetNewRequestActionsSimplifiedResponse>
+    {
+        public int GroupId { get; set; }
+        public string Source { get; set; }
+        public SupportActivityRequest SupportActivity { get; set; }
+    }
+}
---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/GroupService/Response/GetNewRequestActionsSimplifiedResponse.cs
@@ -0,0 +1,13 @@
+﻿using HelpMyStreet.Utils.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace HelpMyStreet.Contracts.GroupService.Response
+{
+    public class GetNewRequestActionsSimplifiedResponse
+    {
+        public Dictionary<Guid,TaskAction> Actions { get; set; }
+
+        public Dictionary<NewTaskAction, List<int>> RequestTaskActions { get; set; }
+    }
+}

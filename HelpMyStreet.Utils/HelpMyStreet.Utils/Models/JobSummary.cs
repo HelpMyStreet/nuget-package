1f779c33d039d6bcf2c7f40a79c9b10612e6b6c0
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2022-02-28 11:32:37 +0000

    Add GetSupportActivityName

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Models/JobSummary.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Models/JobSummary.cs
@@ -1,6 +1,8 @@
 ﻿using HelpMyStreet.Utils.Enums;
+using HelpMyStreet.Utils.Extensions;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace HelpMyStreet.Utils.Models
 {
@@ -13,5 +15,27 @@ namespace HelpMyStreet.Utils.Models
         public int DueDays { get; set; }
         public bool ConsentForContact { get; set; }
         public RequestorType RequestorType { get; set; }
+
+        public string GetSupportActivityName
+        {
+            get
+            {
+                switch (SupportActivity)
+                {
+                    case SupportActivities.Other:
+                        var otherAnswer = Questions.Where(x => x.Id == (int)Enums.Questions.SelectActivity).Select(x => x.Answer).FirstOrDefault();
+                        if(otherAnswer!=null)
+                        {
+                            return otherAnswer;
+                        }
+                        else
+                        {
+                            return SupportActivity.FriendlyNameShort();
+                        }
+                    default:
+                        return SupportActivity.FriendlyNameShort();
+                };
+            }
+        }
     }
 }

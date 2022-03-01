ed5be48a06cd05e1d7365b73228456a80f5e46b1
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2022-03-01 14:59:28 +0000

    Add PutUpdateJobStatusToAppliedFor Request and Response

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Request/PutUpdateJobStatusToAppliedForRequest.cs
@@ -0,0 +1,12 @@
+﻿using HelpMyStreet.Contracts.RequestService.Response;
+using MediatR;
+
+namespace HelpMyStreet.Contracts.RequestService.Request
+{
+    public class PutUpdateJobStatusToAppliedForRequest : IRequest<PutUpdateJobStatusToAppliedForResponse>
+    {
+        public int JobID { get; set; }
+        public int CreatedByUserID { get; set; }
+        public int VolunteerUserID { get; set; }
+    }
+}

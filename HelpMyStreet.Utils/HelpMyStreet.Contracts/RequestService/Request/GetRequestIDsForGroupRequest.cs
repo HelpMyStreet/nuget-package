472b4e41e5b79c95b79c4900468cf9b6ad3a9690
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2022-03-16 09:04:29 +0000

    Add GetRequestIDsForGroup

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Request/GetRequestIDsForGroupRequest.cs
@@ -0,0 +1,11 @@
+﻿using HelpMyStreet.Contracts.RequestService.Response;
+using MediatR;
+
+namespace HelpMyStreet.Contracts.RequestService.Request
+{
+    public class GetRequestIDsForGroupRequest : IRequest<GetRequestIDsForGroupResponse>
+    {
+        public int GroupID { get; set; }
+        public bool IncludeChildGroups { get; set; }
+    }
+}

686f7f4df4febc72dba10ee7b9c2d900beb06027
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2022-07-11 10:59:39 +0100

    Add put methods for approved and rejected

---
+++ HelpMyStreet.Utils/HelpMyStreet.Contracts/RequestService/Request/PutUpdateJobStatusToApprovedRequest.cs
@@ -0,0 +1,12 @@
+﻿using HelpMyStreet.Contracts.RequestService.Response;
+using MediatR;
+
+namespace HelpMyStreet.Contracts.RequestService.Request
+{
+    public class PutUpdateJobStatusToApprovedRequest : IRequest<PutUpdateJobStatusToApprovedResponse>
+    {
+        public int JobID { get; set; }
+        public int CreatedByUserID { get; set; }
+        public int VolunteerUserID { get; set; }
+    }
+}

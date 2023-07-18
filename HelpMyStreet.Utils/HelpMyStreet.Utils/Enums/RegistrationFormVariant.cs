154666100aa2d7e03338da3d0d95a991a9c05e7d
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2023-07-18 16:54:29 +0100

    tidy up of groups

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Enums/RegistrationFormVariant.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Enums/RegistrationFormVariant.cs
@@ -2,25 +2,11 @@
 {
     public enum RegistrationFormVariant
     {
-        Default = 0,
-        FtLOS = 2,
-        AgeUKLSL = 3,
+        Default = 0,        
         FaceMasks = 4,
-        AgeUKWirral = 5,
-        AgeUKNottsBalderton = 6,
-        AgeUKNWK = 7,
-        AgeUKSKC = 8,
-        AgeUKFandS = 9,
-        LincolnshireVolunteers = 10,
-        Ruddington = 11,
-        AgeConnectsCardiff = 12,
-        MeadowsCommunityHelpers = 13,
-        MansfieldCVS = 14,
-        Southwell = 15,
-        ApexBankStaff = 16,
-        AgeUKMidMersey = 17,
-        BostonGNS = 18,
-        UkraineRefugees = 19
+        AgeUKWirral = 5,        
+        Ruddington = 11,        
+        Southwell = 15
     }
 }
     
--- HelpMyStreet.Utils/HelpMyStreet.Utils/Extensions/GroupsExtensions.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Extensions/GroupsExtensions.cs
@@ -10,46 +10,11 @@ namespace HelpMyStreet.Utils.Extensions
         {
             return groups switch
             {
-                Groups.Generic => "HMS",
-                Groups.FTLOS => "FTL",
-                Groups.AgeUKLSL => "LSL",
-                Groups.HLP => "HLP",
-                Groups.Tankersley => "TAP",
+                Groups.Generic => "HMS",                
                 Groups.Ruddington => "RUD",
-                Groups.AgeUKWirral => "WIR",
-                Groups.AgeUKNottsBalderton => "BAL",
-                Groups.AgeUKNorthWestKent => "NWK",
-                Groups.AgeUKNottsNorthMuskham => "NMK",
-                Groups.AgeUKSouthKentCoast => "SKC",
-                Groups.AgeUKFavershamAndSittingbourne => "FAS",
-                Groups.LincolnshireVolunteers => "LCN",
-                Groups.EastLindseyPCN => "EL",
-                Groups.GranthamPCN => "GRA",
-                Groups.SouthLincolnPCN => "SLN",
-                Groups.StamfordPCN => "SFD",
-                Groups.SpilsbyPCN => "SBY",
-                Groups.BostonPCN => "BTN",
-                Groups.LincolnPCN => "APX",
-                Groups.LincolnPortlandPCN => "LPL",
-                Groups.Sandbox => "TEST",
-                Groups.AgeConnectsCardiff => "ACC",
-                Groups.MeadowsCommunityHelpers => "MCC",
-                Groups.MansfieldCVS => "MAN",
-                Groups.MansfieldWickes => "WKS",
-                Groups.GamstonCommunityHall => "GMS",
-                Groups.RichardHerrodCentre => "RHC",
-                Groups.KingsMeadowCampus => "KMC",
-                Groups.ForestRecreationGround => "FRG",
+                Groups.AgeUKWirral => "WIR",                
+                Groups.Sandbox => "TEST",                
                 Groups.Southwell => "SW",
-                Groups.ApexBankStaff => "ABS",
-                Groups.AgeUKMidMersey => "MDM",
-                Groups.BostonGNS => "GNS",
-                Groups.UkraineRefugees => "UKR",
-                Groups.LincolnshireVCS => "VCS",
-                Groups.LincolnshireLCVS=> "LCV",
-                Groups.NHSVRDemo => "NHS",
-                Groups.NottinghamshireCountyCouncil=> "NCC",
-                Groups.NottinghamshireIntegratedCareBoard=>"ICB",
                 _ => throw new ArgumentException(message: $"Unexpected Group: {groups}", paramName: nameof(groups))
             };
         }
@@ -58,21 +23,6 @@ namespace HelpMyStreet.Utils.Extensions
         {
             return groups switch
             {
-                Groups.HLP => false,
-                Groups.GranthamPCN => false,
-                Groups.SouthLincolnPCN => false,
-                Groups.SpilsbyPCN => false,
-                Groups.BostonPCN => false,
-                Groups.LincolnPortlandPCN => false,
-                Groups.MeadowsCommunityHelpers => false,
-                Groups.MansfieldCVS => false,
-                Groups.MansfieldWickes => false,
-                Groups.GamstonCommunityHall => false,
-                Groups.RichardHerrodCentre => false,
-                Groups.KingsMeadowCampus => false,
-                Groups.ForestRecreationGround => false,
-                Groups.BostonGNS => false,
-                Groups.UkraineRefugees => false,
                 _ => true
             };
         }

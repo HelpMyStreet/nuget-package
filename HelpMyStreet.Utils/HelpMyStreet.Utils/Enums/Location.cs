fca3729527358c53aa4b3a457cc42f5528730517
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2023-07-28 08:00:01 +0100

    Remove unused location

--- HelpMyStreet.Utils/HelpMyStreet.Utils/Enums/Location.cs
+++ HelpMyStreet.Utils/HelpMyStreet.Utils/Enums/Location.cs
@@ -2,21 +2,9 @@
 {
     public enum Location
     {
-        LincolnCountyHospital = -1,
-        PilgramHospitalBolton = -2,
         LouthCommunityHospital = -3,
-        TableTennisClubGrantham = -4,
-        WaddingtonBranchSurgerySouthLincoln = -5,
         StMarysMedicalPracticeStamford = -6,
-        FranklinHallSpilsby = -7,
-        SidingsMedicalPracticeBoston = -8,
         RustonsSportsAndSocialClubLincoln = -9,
-        PortlandMedicalPracticeLincoln = -10,
-        MansfieldWickesSite = -11,
-        GamstonCommunityHall = -12,
-        RichardHerrodCentre = -13,
-        KingsMeadowCampus = -14,
-        ForestRecreationGround = -15,
         RichmondMedicalCentre = -16
     }
 }
